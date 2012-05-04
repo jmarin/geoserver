a8a8b6a1b473f0ee0dcdd1a541f2c87c7d25498c
Juan Marin <juan.marin.otero@gmail.com>
2012-05-04 16:02:27 -0400

    Add contact resource

---
+++ src/restconfig/src/main/java/org/geoserver/rest/GlobalContactResource.java
@@ -0,0 +1,33 @@
+package org.geoserver.rest;
+
+import org.geoserver.catalog.rest.AbstractCatalogResource;
+import org.geoserver.config.ContactInfo;
+import org.geoserver.config.GeoServer;
+import org.geoserver.config.util.XStreamPersister;
+import org.geoserver.rest.format.DataFormat;
+import org.geoserver.wfs.WFSInfoImpl;
+import org.restlet.Context;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+
+public class GlobalContactResource extends AbstractCatalogResource {
+
+    private GeoServer geoServer;
+    
+    public GlobalContactResource(Context context, Request request, Response response, Class clazz,
+            GeoServer geoServer) {
+        super(context, request, response, clazz, geoServer.getCatalog());
+        this.geoServer = geoServer;
+    }
+
+    @Override
+    protected Object handleObjectGet() throws Exception {
+        return geoServer.getGlobal().getSettings().getContact();
+    }
+    
+    @Override
+    protected void configurePersister(XStreamPersister persister, DataFormat format) {
+        persister.setHideFeatureTypeAttributes();
+        persister.getXStream().alias("contactinfo", ContactInfo.class);
+    }
+}
