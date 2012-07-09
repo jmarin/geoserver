9f85736132c05b26ae5c2fd0dfd44d0b48feef3e
Juan Marin <juan.marin.otero@gmail.com>
2012-07-09 12:10:14 -0400

    GEOS-5122: REST API enhancements

---
+++ src/restconfig/src/main/java/org/geoserver/service/rest/ServiceSettingsResource.java
@@ -0,0 +1,152 @@
+/* Copyright (c) 2001 - 2012 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.service.rest;
+
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.catalog.rest.AbstractCatalogResource;
+import org.geoserver.config.GeoServer;
+import org.geoserver.config.ServiceInfo;
+import org.geoserver.ows.util.OwsUtils;
+import org.geoserver.rest.RestletException;
+import org.geoserver.wcs.WCSInfo;
+import org.restlet.Context;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
+
+/**
+ * 
+ * @author Juan Marin, OpenGeo
+ * 
+ */
+public class ServiceSettingsResource extends AbstractCatalogResource {
+
+    protected GeoServer geoServer;
+
+    private Class clazz;
+
+    public ServiceSettingsResource(Context context, Request request, Response response,
+            Class clazz, GeoServer geoServer) {
+        super(context, request, response, clazz, geoServer.getCatalog());
+        this.clazz = clazz;
+        this.geoServer = geoServer;
+    }
+
+    @Override
+    public boolean allowPost() {
+        return allowNew();
+    }
+
+    @Override
+    public boolean allowPut() {
+        return allowExisting();
+    }
+
+    @Override
+    public boolean allowDelete() {
+        String workspace = getAttribute("workspace");
+        if (workspace != null) {
+            WorkspaceInfo ws = geoServer.getCatalog().getWorkspaceByName(workspace);
+            return geoServer.getService(ws, clazz) != null;
+        }
+        return false;
+    }
+
+    private boolean allowNew() {
+        String workspace = getAttribute("workspace");
+        WorkspaceInfo ws = geoServer.getCatalog().getWorkspaceByName(workspace);
+        assert (ws != null);
+        return geoServer.getService(ws, clazz) == null;
+    }
+
+    private boolean allowExisting() {
+        String workspace = getAttribute("workspace");
+        if (workspace != null) {
+            WorkspaceInfo ws = geoServer.getCatalog().getWorkspaceByName(workspace);
+            return geoServer.getService(ws, clazz) != null;
+        }
+        return geoServer.getService(clazz) != null;
+    }
+
+    @Override
+    protected Object handleObjectGet() throws Exception {
+        String workspace = getAttribute("workspace");
+        if (workspace != null) {
+            WorkspaceInfo ws = geoServer.getCatalog().getWorkspaceByName(workspace);
+            if (geoServer.getService(ws, clazz) == null) {
+                throw new RestletException("Service for workspace " + workspace + " does not exist",
+                        Status.CLIENT_ERROR_NOT_FOUND);
+            }
+            return geoServer.getService(ws, clazz);
+        }
+        if (geoServer.getService(clazz) == null) {
+            throw new RestletException("Service for workspace " + workspace + " does not exist",
+                    Status.CLIENT_ERROR_NOT_FOUND);
+        }
+        return (ServiceInfo) geoServer.getService(clazz);
+    }
+
+    @Override
+    protected String handleObjectPost(Object object) throws Exception {
+        String name = "";
+        ServiceInfo serviceInfo = handlePost(object);
+        name = serviceInfo.getName();
+        if (name == null) {
+            throw new RestletException("Service name cannot be null",
+                    Status.CLIENT_ERROR_BAD_REQUEST);
+        }
+        geoServer.add(serviceInfo);
+        return name;
+    }
+
+    protected ServiceInfo handlePost(Object object) {
+        String workspace = getAttribute("workspace");
+        if (workspace != null) {
+            WorkspaceInfo ws = geoServer.getCatalog().getWorkspaceByName(workspace);
+            if (ws == null) {
+                throw new RestletException("Workspace does not exist",
+                        Status.CLIENT_ERROR_BAD_REQUEST);
+            }
+            if (geoServer.getService(ws, clazz) != null) {
+                throw new RestletException(
+                        "Service information already exists, creation of a new object is not allowed",
+                        Status.CLIENT_ERROR_FORBIDDEN);
+            }
+            ServiceInfo serviceInfo = (ServiceInfo) object;
+            serviceInfo.setWorkspace(ws);
+            return serviceInfo;
+        }
+        return null;
+    }
+
+    @Override
+    protected void handleObjectPut(Object object) throws Exception {
+        String workspace = getAttribute("workspace");
+        ServiceInfo original = null;
+        if (workspace != null) {
+            WorkspaceInfo ws = geoServer.getCatalog().getWorkspaceByName(workspace);
+            original = geoServer.getService(ws, clazz);
+            OwsUtils.copy(object, original, clazz);
+            original.setWorkspace(ws);
+        } else {
+            original = geoServer.getService(clazz);
+        }
+        OwsUtils.copy(object, original, clazz);
+        geoServer.save(original);
+    }
+
+    @Override
+    protected void handleObjectDelete() throws Exception {
+        String workspace = getAttribute("workspace");
+        if (workspace != null) {
+            WorkspaceInfo ws = geoServer.getCatalog().getWorkspaceByName(workspace);
+            ServiceInfo serviceInfo = geoServer.getService(ws, clazz);
+            if (serviceInfo != null) {
+                geoServer.remove(serviceInfo);
+            }
+        }
+    }
+}
---
+++ src/restconfig/src/test/java/org/geoserver/service/rest/LocalWFSSettingsTest.java
@@ -0,0 +1,134 @@
+package org.geoserver.service.rest;
+
+import static org.custommonkey.xmlunit.XMLAssert.assertXpathEvaluatesTo;
+import net.sf.json.JSON;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.catalog.rest.CatalogRESTTestSupport;
+import org.geoserver.config.GeoServer;
+import org.geoserver.ows.LocalWorkspace;
+import org.geoserver.platform.GeoServerExtensions;
+import org.geoserver.wfs.WFSInfo;
+import org.w3c.dom.Document;
+
+import com.mockrunner.mock.web.MockHttpServletResponse;
+
+public class LocalWFSSettingsTest extends CatalogRESTTestSupport {
+
+    protected GeoServer geoServer;
+
+    @Override
+    protected void setUpInternal() throws Exception {
+        super.setUpInternal();
+        geoServer = GeoServerExtensions.bean(GeoServer.class, applicationContext);
+        WorkspaceInfo ws = geoServer.getCatalog().getWorkspaceByName("sf");
+        LocalWorkspace.set(ws);
+        WFSInfo wfsInfo = geoServer.getService(WFSInfo.class);
+        wfsInfo.setWorkspace(ws);
+        geoServer.save(wfsInfo);
+    }
+
+    @Override
+    protected void tearDownInternal() throws Exception {
+        LocalWorkspace.remove();
+    }
+
+    public void testGetAsJSON() throws Exception {
+        JSON json = getAsJSON("/rest/services/wfs/sf/settings.json");
+        JSONObject jsonObject = (JSONObject) json;
+        assertNotNull(jsonObject);
+        JSONObject wfsinfo = (JSONObject) jsonObject.get("wfsinfo");
+        assertEquals("wfs", wfsinfo.get("id"));
+        assertEquals("My GeoServer WFS", wfsinfo.get("name"));
+        JSONObject workspace = (JSONObject) wfsinfo.get("workspace");
+        assertNotNull(workspace);
+        assertEquals("sf", workspace.get("name"));
+        assertEquals("COMPLETE", wfsinfo.get("serviceLevel"));
+        assertEquals("1000000", wfsinfo.get("maxFeatures").toString().trim());
+    }
+
+    public void testGetAsXML() throws Exception {
+        Document dom = getAsDOM("/rest/services/wfs/sf/settings.xml");
+        assertEquals("wfsinfo", dom.getDocumentElement().getLocalName());
+        assertXpathEvaluatesTo("true", "/wfsinfo/enabled", dom);
+        assertXpathEvaluatesTo("sf", "/wfsinfo/workspace/name", dom);
+        assertXpathEvaluatesTo("My GeoServer WFS", "/wfsinfo/name", dom);
+        assertXpathEvaluatesTo("COMPLETE", "/wfsinfo/serviceLevel", dom);
+        assertXpathEvaluatesTo("1000000", "/wfsinfo/maxFeatures", dom);
+    }
+
+    public void testPostAsJSON() throws Exception {
+        removeLocalWorkspace();
+        String input = "{'wfsinfo': {'id' : 'wfs', 'name' : 'WFS', 'workspace': {'name': 'sf'},'enabled': 'true'}}";
+        MockHttpServletResponse response = postAsServletResponse("/rest/services/wfs/sf/settings/",
+                input, "text/json");
+        assertEquals(201, response.getStatusCode());
+        JSON json = getAsJSON("/rest/services/wfs/sf/settings.json");
+        JSONObject jsonObject = (JSONObject) json;
+        assertNotNull(jsonObject);
+        JSONObject wmsinfo = (JSONObject) jsonObject.get("wfsinfo");
+        assertEquals("wfs", wmsinfo.get("id"));
+        assertEquals("WFS", wmsinfo.get("name"));
+        JSONObject workspace = (JSONObject) wmsinfo.get("workspace");
+        assertNotNull(workspace);
+        assertEquals("sf", workspace.get("name"));
+    }
+
+    public void testPostAsXML() throws Exception {
+        removeLocalWorkspace();
+        String xml = "<wfsinfo>" + "<id>wfs</id>" + "<workspace>" + "<name>sf</name>"
+                + "</workspace>" + "<name>OGC:WFS</name>" + "<enabled>false</enabled>"
+                + "</wfsinfo>";
+        MockHttpServletResponse response = postAsServletResponse("/rest/services/wfs/sf/settings",
+                xml, "text/xml");
+        assertEquals(201, response.getStatusCode());
+
+        Document dom = getAsDOM("/rest/services/wfs/sf/settings.xml");
+        assertEquals("wfsinfo", dom.getDocumentElement().getLocalName());
+        assertXpathEvaluatesTo("false", "/wfsinfo/enabled", dom);
+        assertXpathEvaluatesTo("sf", "/wfsinfo/workspace/name", dom);
+        assertXpathEvaluatesTo("OGC:WFS", "/wfsinfo/name", dom);
+    }
+
+    public void testPutAsJSON() throws Exception {
+        String json = "{'wfsinfo': {'id':'wfs','workspace':{'name':'sf'},'enabled':'false','name':'WFS'}}";
+        MockHttpServletResponse response = putAsServletResponse("/rest/services/wfs/sf/settings/",
+                json, "text/json");
+        assertEquals(200, response.getStatusCode());
+        JSON jsonMod = getAsJSON("/rest/services/wfs/sf/settings.json");
+        JSONObject jsonObject = (JSONObject) jsonMod;
+        assertNotNull(jsonObject);
+        JSONObject wfsinfo = (JSONObject) jsonObject.get("wfsinfo");
+        assertEquals("wfs", wfsinfo.get("id"));
+        assertEquals("false", wfsinfo.get("enabled").toString().trim());
+    }
+
+    public void testPutAsXML() throws Exception {
+        String xml = "<wfsinfo>" + "<id>wfs</id>" + "<workspace>" + "<name>sf</name>"
+                + "</workspace>" + "<enabled>false</enabled>" + "</wfsinfo>";
+        MockHttpServletResponse response = putAsServletResponse("/rest/services/wfs/sf/settings",
+                xml, "text/xml");
+        assertEquals(200, response.getStatusCode());
+        Document dom = getAsDOM("/rest/services/wfs/sf/settings.xml");
+        assertXpathEvaluatesTo("false", "/wfsinfo/enabled", dom);
+    }
+
+    public void testDelete() throws Exception {
+        assertEquals(200, deleteAsServletResponse("/rest/services/wfs/sf/settings").getStatusCode());
+        boolean thrown = false;
+        try {
+            JSON json = getAsJSON("/rest/services/wfs/sf/settings.json");
+        } catch (JSONException e) {
+            thrown = true;
+        }
+        assertEquals(true, thrown);
+    }
+
+    private void removeLocalWorkspace() {
+        WorkspaceInfo ws = geoServer.getCatalog().getWorkspaceByName("sf");
+        WFSInfo wfsInfo = geoServer.getService(ws, WFSInfo.class);
+        geoServer.remove(wfsInfo);
+    }
+}
